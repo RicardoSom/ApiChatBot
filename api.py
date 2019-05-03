55dbdf0b682499747ac57c927983785fe099efc6
RicardoSom <r.orellana.munoz@gmail.com>
2019-05-02 20:09:51 -0400

    Primer commit de la api

---
+++ api.py
@@ -0,0 +1,34 @@
+import flask
+from flask import request
+import aiml
+import pyttsx3
+import os
+from flask_cors import CORS
+
+app = flask.Flask(__name__)
+CORS(app)
+app.config["DEBUG"] = True
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for a chatbot using python and AIML</p></br><p>Created by Ricardo Orellana RSOM </p>"
+
+@app.route('/bot', methods=['GET'])
+def API():
+    # Create the kernel and learn AIML files
+    kernel = aiml.Kernel()
+    if os.path.isfile("bot_brain.brn"):
+      kernel.bootstrap(brainFile = "bot_brain.brn")
+    else:
+      kernel.bootstrap(learnFiles = "zoe-startup.xml", commands = "load aiml b")
+      kernel.saveBrain("bot_brain.brn")
+
+    data = request.args.get('input')
+
+
+    zoes_response = kernel.respond(data)
+    
+    return zoes_response 
+
+app.run()
